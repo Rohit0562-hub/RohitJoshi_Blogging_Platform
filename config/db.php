a667e8261e4028408c1d56d9bb41095ab084c061
Rohit Joshi <rohitjoshi05.np@gmail.com>
2026-01-22 22:33:51 +0545

    Added Database Tables

--- config/db.php
+++ config/db.php
@@ -6,7 +6,13 @@ function dbConnect() {
 	$password = "";
 
 	try {
-		$con = new PDO($server, $user, $password);
+		$options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false
+    ];
+    
+		$con = new PDO($server, $user, $password, $options);
 
 		return $con;
 	} catch (PDOException $e) {
